7bf765782c3066539c3742cf99b62ec2372b3408
Artem Mozhaev <tema.mozhaev@gmail.com>
2023-09-19 13:52:03 +0200

    Header fix

--- src/components/General/Header.tsx
+++ src/components/General/Header.tsx
@@ -1,16 +1,16 @@
+import React from "react";
 import Logo from "../../assets/images/logo.png";
 
-const Header = () => {
-  
-return (
-    <header className="header">
-    <div className="stiky__header">
-      <div className="logo__header">
-        <Logo />
-      </div>
-      </div>
-      </header>
-)
-}
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <div className="stiky__header">
+                <div className="logo__header">
+                    <img src={Logo} />
+                </div>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
